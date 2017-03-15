5d53c1bd73b7f8cde94b45d1495043a394fb179f
xiaohongyang <xiaohongyang@adsage.com>
2017-03-15 10:56:11 +0800

    table components

--- resources/views/mail/invoice/paid.blade.php
+++ resources/views/mail/invoice/paid.blade.php
@@ -7,6 +7,17 @@
 Button Text
 @endcomponent
 
+@component('mail::table')
+| Laravel       | Table         | Example  |
+| ------------- |:-------------:| --------:|
+| Col 2 is      | Centered      | $10      |
+| Col 3 is      | Right-Aligned | $20      |
+@endcomponent
+
 Thanks,<br>
 {{ config('app.name') }}
+@endcomponent
+
+@component('mail::panel')
+This is panel component
 @endcomponent

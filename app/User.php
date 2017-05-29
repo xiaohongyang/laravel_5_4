7dcea7c6923a7e9324153275eb2c1b7c910cbc17
xiaohongyang <258082291@qq.com>
2017-05-29 10:45:10 +0800

    user api_token

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'api_token'
     ];
 
     /**
@@ -32,7 +32,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'api_token'
     ];
 
 
